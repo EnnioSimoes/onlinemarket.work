88eb752841829d1e0da6f9b46831bd8d425228ff
Ennio <ennio.simoes@outlook.com>
2015-03-05 23:58:13 -0300

    Resolução: M7Ex1

--- module/Market/config/module.config.php
+++ module/Market/config/module.config.php
@@ -1,51 +1,92 @@
 <?php
 
- return array(
+return array(
     'router' => array(
         'routes' => array(
+            'home' => array(
+                'type' => 'Literal',
+                'options' => array(
+                    'route' => '/',
+                    'defaults' => array(
+                        'controller' => 'market-index-controller',
+                        'action' => 'index'
+                    )
+                )
+            ),
             'market' => array(
                 'type' => 'Literal',
                 'options' => array(
-                    'route'    => '/market',
+                    'route' => '/market',
                     'defaults' => array(
                         'controller' => 'market-index-controller',
-                        'action'     => 'index',
+                        'action' => 'index'
                     ),
                 ),
                 'may_terminate' => true,
                 'child_routes' => array(
-                    'default' => array(
-                        'type'    => 'Segment',
+                    'view' => array(
+                        'type' => 'Literal',
                         'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
+                            'route' => '/view',
                             'defaults' => array(
+                                'controller' => 'market-view-controller',
+                                'action' => 'index'
+                            )
+                        ),
+                        'may_terminate' => true,
+                        'child_routes' => array(
+                            'main' => array(
+                                'type' => 'Segment',
+                                'options' => array(
+                                    'route' => '/main[/:category]',
+                                    'defaults' => array(
+                                        'action' => 'index'
+                                    ),
+                                ),
+                            ),
+                            'item' => array(
+                                'type' => 'Segment',
+                                'options' => array(
+                                    'route' => '/item[/:itemId]',
+                                    'defaults' => array(
+                                        'action' => 'item'
+                                    ),
+                                    'constraints' => array(
+                                        'itemId' => '[0-9]*'
+                                    )
+                                ),
                             ),
                         ),
                     ),
-                ),                
-            ),
-        ),
-    ),     
+                    'post' => array(
+                        'type' => 'Literal',
+                        'options' => array(
+                            'route' => '/post',
+                            'defaults' => array(
+                                'controller' => 'market-post-controller',
+                                'action' => 'index'
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    ),
     'controllers' => array(
         'invokables' => array(
             'market-index-controller' => 'Market\Controller\IndexController',
             'market-view-controller' => 'Market\Controller\ViewController'
         ),
-        'factories' =>array(
+        'factories' => array(
             'market-post-controller' => 'Market\Factory\PostControllerFactory'
         ),
-        'aliases' =>array(
+        'aliases' => array(
             'alt' => 'market-view-controller'
         )
-    ),     
+    ),
     'view_manager' => array(
         'template_path_stack' => array(
             __DIR__ . '/../view',
         ),
-    ),  
-     
- );
+    ),
+);
